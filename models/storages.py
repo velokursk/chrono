a0a94d13a85b35d6284bd419be30e4a2513c1716
40iN_KoS <mr40@ya.ru>
2017-01-18 10:09:09 +0300

    #CR-8 SingletonRamStorage

--- models/storages.py
+++ models/storages.py
@@ -2,24 +2,35 @@ class Storage(object):
     def set(self, model):
         pass
 
-    def get(self, **query):
+    def get(self, model_cls, **query):
         pass
 
     def drop(self, models):
         pass
 
 
-class RamStorage(object):
+class SingletonRamStorage(object):
     items = {}
 
     def set(self, model):
-        pass
+        storage = self._get_model_storage(model.__class__)
+        storage[model.pk] = model
 
-    def get(self, **query):
-        pass
+    def get(self, model_cls, **query):
+        storage = self._get_model_storage(model_cls)
+        models = storage.values()
+        for attr, value in query.iteritems():
+            models = [
+                model
+                for model in models
+                if getattr(model, attr) == value
+            ]
+        return models
 
-    def drop(self, models):
-        pass
+    def drop(self, *models):
+        for model in models:
+            storage = self._get_model_storage(model.__class__)
+            del storage[model.pk]
 
     def _get_model_storage(self, model):
         model_key = model.__class__
@@ -28,6 +39,3 @@ class RamStorage(object):
         except KeyError:
             self.items[model_key] = {}
             return self.items[model_key]
-
-    def _get_model_pk(self, model):
-        pass
